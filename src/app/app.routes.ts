b24322e044c4f34b6f81ec9b7092a49c475756b2
ShuaJay <joshuajayphiri5@gmail.com>
2026-02-16 16:32:20 +0200

    add homepage as the default landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { LucideAngularModule, Shirt } from 'lucide-angular';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { LucideAngularModule, Shirt } from 'lucide-angular';
     RouterOutlet,
     LucideAngularModule,
     MatSlideToggleModule,
-  ],
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
