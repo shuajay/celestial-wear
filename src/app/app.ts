17cb731e924d163cfc5c3686d60b54b80475f694
ShuaJay <joshuajayphiri5@gmail.com>
2026-02-16 15:03:10 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LucideAngularModule, Shirt } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    LucideAngularModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
