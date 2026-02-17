499584af6992475e799f5518de939cc22a6b3247
ShuaJay <joshuajayphiri5@gmail.com>
2026-02-17 14:20:07 +0200

    toolbar not yet complete and integration of primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 
@@ -9,5 +10,15 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimations(),
+    providePrimeNG({
+      ripple: true,
+      inputStyle: 'outlined',
+      theme: {
+        preset: 'saga-blue',
+        options: {
+          darkModeSelector: '.dark-mode',
+        }
+      }
+    })
   ]
 };
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HeaderComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
