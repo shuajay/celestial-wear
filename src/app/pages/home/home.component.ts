59f19d066b2b69ac34040430774c4e72420eb677
ShuaJay <joshuajayphiri5@gmail.com>
2026-03-04 14:31:11 +0200

    add banner on home-page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-home',
   imports: [
     HeaderComponent,
-  ],
+    Button
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
